b444a1c0c2c7ef9a35605d60d5b82bc56466f4f5
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-11-06 17:52:14 +0100

    first load venv

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
-# get postgres credentials from azure vault and set them as environment variables
-python azure_credentials.py
-
 # activate virtual env
 source $VIRTUAL_ENV/bin/activate
+
+# get postgres credentials from azure vault and set them as environment variables
+python azure_credentials.py
 
 # initialize django app
 python manage.py collectstatic --noinput
