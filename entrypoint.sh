29fe57ab02a714660a9f2e5a94665439f3f5ce46
sebastianneubauer <sebineubauer@gmail.com>
2020-11-09 11:56:39 +0100

    Add collect static command to entrypoint (#10)
    Add collect static command to entrypoint 
    
    Co-authored-by: Sebastian Neubauer <sebastian.neubauer@jda.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 source $VIRTUAL_ENV/bin/activate
+python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py initadmin
