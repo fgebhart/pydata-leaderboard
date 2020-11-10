1e73ebf676c4f9da080c5803db271e5aca9136f7
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-11-10 10:36:22 +0100

    change ordering of score and make it float

--- ranking_app/models.py
+++ ranking_app/models.py
@@ -6,7 +6,7 @@ class Ranking(models.Model):
     def __str__(self):
         return f"{self.user}: {self.score}"
 
-    user = models.CharField(max_length=100, unique=True)
-    score = models.IntegerField()
+    user = models.CharField(max_length=200, unique=True)
+    score = models.FloatField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,10 +2,10 @@ from ranking_app import models
 
 
 def test_ranking(db):
-    ranking = models.Ranking(user="Player1", score=123)
+    ranking = models.Ranking(user="Player1", score=123.45)
     ranking.save()
     assert ranking.user == "Player1"
-    assert ranking.score == 123
+    assert ranking.score == 123.45
     ranking_object = models.Ranking.objects.get(user="Player1")
     assert ranking_object.user == "Player1"
-    assert ranking_object.score == 123
+    assert ranking_object.score == 123.45
